6279e19a35c94d4679053cbfe2831e48219d9469
JBert9000 <jbertlik@gmail.com>
2018-05-13 13:20:24 -0400

    created a dynamic show page with params[:id]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get 'entries/index'
+  #
+  # get 'entries/create'
+  #
+  # get 'entries/new'
+  #
+  # get 'entries/edit'
+  #
+  # get 'entries/show'
+  #
+  # get 'entries/update'
+  #
+  # get 'entries/destroy'
+
   resources :entries
 end
